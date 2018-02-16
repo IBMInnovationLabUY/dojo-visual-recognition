c66a8b7075077c33a37c13f72ef299a78b4d057b
Juan Pablo Fajardo Fontes <juan.fajardo@ibm.com>
2018-02-16 09:41:07 -0300

    dojo finalizado y subido a box

---
+++ visualRecognition/visualRecognition/delete.js
@@ -0,0 +1,16 @@
+var watson = require('watson-developer-cloud');
+
+var visual_recognition = watson.visual_recognition({
+  api_key: '507d8824e898d272961f7b5f7a63ff4de056868e',
+  version: 'v3',
+  version_date: '2016-05-20'
+});
+
+visual_recognition.deleteClassifier({
+  classifier_id: 'dog_1313165534' },
+  function(err, response) {
+    if (err)
+      console.log(err);
+    else
+      console.log(JSON.stringify(response, null, 2))
+});
